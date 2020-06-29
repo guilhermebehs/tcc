25b771afe7e7dc4618963fd5ac37787aea1e06c7
guilherme <guilhermebehs2013@hotmail.com>
2020-06-29 19:40:51 -0300

    adicionado método que retorna por raça

--- controlador_redis.py
+++ controlador_redis.py
@@ -11,23 +11,44 @@ import redis
 def iniciarClienteRedis():
 	r = redis.Redis()
 	return r
-
+    
 
 def retornarChaves(expressaoRegular):
+   resultado = {}
+   for key in clienteRedis.scan_iter(expressaoRegular):
+   	   resultado[key] = clienteRedis.hgetall(key)
+   
+   return resultado
 
-def retornarValores(chaves):
+def mergeResultados(resultado, dictsParaMerge):
+    for dic in dictsParaMerge:
+        resultado.update(dic)
+
+    return resultado
 
 
-def retornarPorAno():
 
-def retornarPorIntervaloAno():
+def retornarPorRaca(racas):
+    resultados=[]
+    for raca in racas:
+        resultados.append(retornarChaves("*-"+raca))
 
-def retornarPorIntervaloAnoESexo():
+    if len(resultados) > 0:
+    	return mergeResultados(resultados[0], resultados)
+    else:
+        return {}
 
+#def retornarPorIntervaloAno():
+
+#def retornarPorIntervaloAnoESexo():
 
 clienteRedis = iniciarClienteRedis()
 
 def main ():
+    resultados = retornarPorRaca(["Parda", "Preta"])
+    for resultado in resultados:
+        print(resultado)
+
 
 
 if __name__ == '__main__':
